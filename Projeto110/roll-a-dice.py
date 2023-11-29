574647085fd95348b99c31921a85cccfe18b7a85
joanahae <111794333+joanahae@users.noreply.github.com>
2023-11-29 16:52:30 -0300

    Add files via upload

---
+++ Projeto110/roll-a-dice.py
@@ -0,0 +1,50 @@
+import random
+
+#input=("quer jogar o dado? y ou n")
+
+resposta = "y"
+
+while resposta == "y":
+    no = random.randint(1,6)
+
+if no ==1:
+    print("[-----]")
+    print("[     ]")
+    print("[  0  ]")
+    print("[     ]")
+    print("[-----]")
+
+if no ==2:
+    print("[-----]")
+    print("[ 0   ]")
+    print("[     ]")
+    print("[   0 ]")
+    print("[-----]")
+
+if no ==3:
+    print("[-----]")
+    print("[ 0   ]")
+    print("[  0  ]")
+    print("[   0 ]")
+    print("[-----]")
+
+if no ==4:
+    print("[-----]")
+    print("[0   0]")
+    print("[     ]")
+    print("[0   0]")
+    print("[-----]")
+
+if no ==5:
+    print("[-----]")
+    print("[0   0]")
+    print("[  0  ]")
+    print("[0   0]")
+    print("[-----]")
+
+if no ==6:
+    print("[-----]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[-----]")
